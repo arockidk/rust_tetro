4ba4e45c09bc57121c8b10e280f39b141eabfff0
arockidk <joeykicker2018@gmail.com>
2023-11-07 07:03:09 +1100

    quick fumen update

--- src/fumen/mod.rs
+++ src/fumen/mod.rs
@@ -1,8 +1,16 @@
 use crate::field::Field;
 pub fn encodeFumen(field: &Field) {
     let mut base: String = String::from("v115@");
+    let mut count: i64 = 0;
+    let mut previous_tile: i8 = 0;
     for tile in field.get_tile_array() {
-        
+        if previous_tile == tile {
+            count += 1;
+        } else {
+            
+            previous_tile = tile;
+        }
+
     }
     
 }
